c960c6a6b7124f04bd327676f80e42f0a599f992
gofordivya <gofordivya@gmail.com>
2023-12-15 23:19:46 +0100

    created static form page

---
+++ src/components/TaskForm.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class TaskForm extends Component {
+  render() {
+    return (
+      <div>
+        <h2 className="mb-4 mt-4">New Task:</h2>
+        <div className="form-group mb-3">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Your task"
+          ></input>
+        </div>
+        <div className="form-group mb-3">
+          <textarea
+            className="form-control"
+            rows="3"
+            placeholder="Describe it"
+          ></textarea>
+        </div>
+        <button type="button" className="btn btn-primary">
+          Add
+        </button>
+      </div>
+    );
+  }
+}
+
+export default TaskForm;
