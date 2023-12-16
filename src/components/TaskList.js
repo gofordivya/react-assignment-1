641212b6b5a6cb2f2487bf1f303932005fdc177a
gofordivya <gofordivya@gmail.com>
2023-12-16 19:06:27 +0100

    implemented delete

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 
 class TaskList extends Component {
+  handleDeleteTask = (id) => {
+    const existingTaskList = JSON.parse(localStorage.getItem("taskList")) || [];
+
+    const taskItemindexToDelete = existingTaskList.findIndex(
+      (task) => task.id === id
+    );
+
+    if (taskItemindexToDelete !== -1) {
+      existingTaskList.splice(taskItemindexToDelete, 1);
+      localStorage.setItem("taskList", JSON.stringify(existingTaskList));
+    }
+  };
   render() {
     const { taskList } = this.props;
     return (
@@ -10,7 +22,12 @@ class TaskList extends Component {
             <li key={item.id} className="list-group-item">
               <b>{item.title} </b>
               <em>{item.description}</em>
-              <a className="btn btn-success float-end">Done</a>
+              <a
+                className="btn btn-success float-end"
+                onClick={() => this.handleDeleteTask(item.id)}
+              >
+                Done
+              </a>
             </li>
           ))}
         </ul>
